8bb788673355d5581580b06f48416ba4c566cdb1
mihir20 <mihir20121997@gmail.com>
2017-12-17 15:26:22 +0530

    authentication failing handled

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import Card from './common/Card';
 import CardItem from './common/CardItem';
 import Button from './common/Button';
 import Input from './common/Input';
 
+
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
+
+onButtonPress() {
+  const { email, password } = this.state;
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .catch(() => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch(() => {
+      this.setState({ error: 'Authentication Failed' });
+    }
+  );
+  }
+  );
+}
+
+
   render() {
     return (
       <Card>
@@ -26,13 +44,24 @@ value={this.state.password}
 onChangeText={password => this.setState({ password })}
 />
         </ CardItem >
-
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
         <CardItem>
-          <Button>Log In</Button>
+
+          <Button
+          onPress={this.onButtonPress.bind(this)}
+          >Log In</Button>
         </CardItem>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    color: 'red',
+    fontSize: 20,
+    alignSelf: 'center'
+  }
+};
 
 export default LoginForm;
