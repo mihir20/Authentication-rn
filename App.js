35b78dcbc59d12f1ab29f799edd9b85e9670b750
mihir20 <mihir20121997@gmail.com>
2017-12-18 02:12:49 +0530

    logout event added

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 import Header from './src/components/common/Header';
+import Button from './src/components/common/Button';
 import LoginForm from './src/components/LoginForm';
+import LoadingSpinner from './src/components/common/LoadingSpinner';
 
 class App extends Component {
+
+
+  state = { logIn: null } ;
+
+
   componentWillMount() {
     firebase.initializeApp({
     apiKey: 'AIzaSyA8Nwkhgd4D142V_E0aDUZQ3UcDe7wC2k8',
@@ -20,15 +21,39 @@ class App extends Component {
     storageBucket: '',
     messagingSenderId: '239509440223'
   });
-  }
-  render() {
-    return (
-      <View >
-      <Header headerText="Authentication" />
-      <LoginForm />
-      </View>
-    );
-  }
-}
 
-export default App;
+  firebase.auth().onAuthStateChanged((user) => {
+       if (user) {
+         this.setState({ loggedIn: true });
+       } else {
+         this.setState({ loggedIn: false });
+       }
+     });
+   }
+
+   renderContent() {
+     switch (this.state.loggedIn) {
+       case true:
+         return (<View style={{ height: 40 }}>
+           <Button onPress={() => firebase.auth().signOut()}>
+             Logout
+           </Button>
+         </View>);
+       case false:
+         return <LoginForm />;
+       default:
+         return <LoadingSpinner size="large" />;
+     }
+   }
+
+   render() {
+     return (
+       <View>
+         <Header headerText="Authentication" />
+         {this.renderContent()}
+       </View>
+     );
+   }
+ }
+
+ export default App;
